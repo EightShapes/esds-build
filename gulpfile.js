3822f4d86e2a6db2a18df2f7af01a86eefd5a56e
Kevin Powell <kevin@eightshapes.com>
2017-08-17 09:42:19 -0500

    Reduce complexity of gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,4 @@
-const   configFile = './tests/sample_project/build-config.js',
-        taskFiles = './tasks/*.js',
-        gulp = require('gulp'),
-        HubRegistry = require('gulp-hub');
+const buildConfig = `./tests/sample_project/build-config.js`;
+module.exports.config = require(buildConfig);
 
-module.exports.config = require(configFile);
-        
-/* load some gulpfiles into the registry */
-var hub = new HubRegistry([taskFiles]);
-
-/* tell gulp to use the tasks just loaded */
-gulp.registry(hub);
-
-
-/**************************************************/
-/* Composite tasks ********************************/
-/**************************************************/
-
-// Watch everything
-gulp.task('watch:all', gulp.parallel('watch:styles:all', 'watch:markup:all', 'watch:serve:all'));
-
-//Build everything
-gulp.task('build:all', gulp.parallel('styles:build:all', gulp.series('markup:concatenate-macros:all', 'markup:build:all')));
-
-// Build and serve the project, watch for changes to files
-gulp.task('default', gulp.series('build:all', gulp.parallel('watch:all', 'serve:local-docs')));
-
+const gulp = require('./tasks/uds_tasks.js');
---
+++ tasks/uds_tasks.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const rootPath = process.cwd(),
+        taskFiles = ['**/*.js', '!uds_tasks.js'],
+        gulp = require(`${rootPath}/node_modules/gulp`), // Need to make sure the "gulp" loaded is the gulp installed in ./node_modules/gulp, not the one installed in this package
+        HubRegistry = require('gulp-hub');
+
+/* load some gulpfiles into the registry */
+var hub = new HubRegistry(taskFiles);
+
+
+/* tell gulp to use the tasks just loaded */
+gulp.registry(hub);
+/**************************************************/
+/* Composite tasks ********************************/
+/**************************************************/
+
+// Watch everything
+gulp.task('watch:all', gulp.parallel('watch:styles:all', 'watch:markup:all', 'watch:serve:all'));
+
+//Build everything
+gulp.task('build:all', gulp.parallel('styles:build:all', gulp.series('markup:concatenate-macros:all', 'markup:build:all')));
+
+// Build and serve the project, watch for changes to files
+gulp.task('default', gulp.series('build:all', gulp.parallel('watch:all', 'serve:local-docs')));
+
+module.exports = gulp;
