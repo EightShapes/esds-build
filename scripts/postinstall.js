864e15f110cde4c269da54be9856ecc613ad3cfa
Kevin Powell <kevin@eightshapes.com>
2017-08-30 13:57:37 -0500

    Add postinstall script

---
+++ scripts/postinstall.js
@@ -0,0 +1,12 @@
+'use strict';
+const path = require('path'),
+        fs = require('fs'),
+    { exec } = require('child_process'),
+    packageJsonPath = path.join(process.cwd(), 'package.json'),
+    packageConfig = require(packageJsonPath);
+
+exec(`npm install gulp-cli --save-dev`);
+exec(`npm install git+https://github.com/gulpjs/gulp.git#4.0 --save-dev`);
+
+packageConfig.scripts.start = 'gulp';
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageConfig));
