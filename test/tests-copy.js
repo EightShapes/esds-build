33fcde015f4acd542c328f2c66482a1a701f16a1
Kevin Powell <kevin@eightshapes.com>
2017-09-26 14:38:16 -0500

    Fix watch task

--- test/tests-copy.js
+++ test/tests-copy.js
@@ -4,27 +4,13 @@
 /* global beforeEach */
 
 'use strict';
-const { exec } = require('child_process'),
-        assert = require('yeoman-assert'),
+const assert = require('yeoman-assert'),
         gulp = require('./tests-gulp.js'),
         fs = require('fs'),
         mkdirp = require('mkdirp'),
         projectPath = './test/sample_project',
         webroot = `${projectPath}/_site/latest`,
         webrootImages = `${webroot}/images`;
-
-// TODO Move this function to a commonly shared place
-function recursivelyCheckForFiles(filePaths, done) {
-  let allFilesFound = filePaths.every(file => fs.existsSync(file));
-
-  if (allFilesFound) {
-    done();
-  } else {
-    setTimeout(function() {
-      recursivelyCheckForFiles(filePaths, done);
-    }, 20);
-  }
-}
 
 module.exports = function(){
     describe('copy', function(){
@@ -43,14 +29,14 @@ module.exports = function(){
             });
         });
 
-        describe('watching files for changes and copying when updated', function(){
-            it('should sense changes to copied files and re-copy when updated', function(done) {
-                exec('gulp watch:copy:all'); // start watch
-                gulp('clean:webroot') // clear webroot
-                    .then(result => {
-                      exec(`touch ${projectPath}/images/eightshapes_logo.png`);
-                      recursivelyCheckForFiles([`${webrootImages}/eightshapes_logo.png`], done);
-                });
+        describe('watch task generation', function(){
+            it('should have a watch task to monitor changes to copied files and re-copy when updated', function() {
+                return gulp('--tasks')
+                  .then(result => {
+                    assert(result.stdout.includes('watch:copy:images'));
+                    assert(result.stdout.includes('watch:copy:dist'));
+                    assert(result.stdout.includes('watch:copy:random-dependencies'));
+                  });
             });
         });
 
